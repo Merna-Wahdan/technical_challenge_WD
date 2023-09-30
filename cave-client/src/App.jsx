1141048511e832ef90cf9cc830f9222fe41e5a8d
Merna <merna.wahdan@gmail.com>
2023-09-30 20:51:35 +0200

    Render phones & show details on click

--- cave-client/src/App.jsx
+++ cave-client/src/App.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [phones, setPhones] = useState([]);
+  const [selectedPhone, setSelectedPhone] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/phones`)
+      .then((res) => {
+        console.log("resspones", res);
+        setPhones(res.data);
+      })
+      .catch((e) => console.log("Error get phones", e));
+  }, []);
+
+  const handleSelected = (phone) => {
+    console.log(phone);
+    setSelectedPhone(phone);
+  };
+
+  return (
+    <>
+      <h1>Phones list</h1>
+      {phones.map((e) => {
+        return (
+          <ul key={e.id}>
+            <li onClick={() => handleSelected(e)}>{e.name}</li>
+          </ul>
+        );
+      })}
+      <div>
+        <h2>Selected phone</h2>
+        <h4>Name: {selectedPhone.name}</h4>
+        <h4>manufacturer: {selectedPhone.manufacturer}</h4>
+        <h4>description: {selectedPhone.description}</h4>
+        <h4>color: {selectedPhone.color}</h4>
+        <h4>imageFileName: {selectedPhone.imageFileName}</h4>
+        <h4>screen: {selectedPhone.screen}</h4>
+        <h4>processor: {selectedPhone.processor}</h4>
+        <h4>ram: {selectedPhone.ram}</h4>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- cave-server/app.js
+++ cave-server/app.js
@@ -9,16 +9,23 @@ var usersRouter = require("./routes/users");
 const phonesRouter = require("./routes/phone");
 
 var app = express();
+var cors = require("cors");
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -39,5 +46,9 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+const originUrl = process.env.ORIGIN || "http://127.0.0.1:5173";
+
+app.use(cors());
 
 module.exports = app;
